f0ff53af806c703f852e5e699069efffcf268d06
emanegux <dev.evanpiro@gmail.com>
2021-02-10 11:52:27 -0500

    added auth

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
 import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
+import Auth from '@aws-amplify/auth'
 
-import OktaSignIn from '@okta/okta-signin-widget';
-// import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
+Auth.configure({
+
+  // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+  identityPoolId: 'us-east-1:8ec0ca2c-4e7d-4f5e-9755-ad27a946f885',
+
+  // REQUIRED - Amazon Cognito Region
+  region: 'us-east-1',
+
+  // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+  // Required only if it's different from Amazon Cognito Region
+  identityPoolRegion: 'us-east-1',
+
+  // OPTIONAL - Amazon Cognito User Pool ID
+  userPoolId: 'us-east-1_L04zA9HKx',
+
+  // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  userPoolWebClientId: '6bhp15ot1l2tqe849ikq06hvet',
+})
 
 
-const signIn = new OktaSignIn({
-  baseUrl: 'https://dev-23201487.okta.com',
-  clientId: '0oa58u4h8mlrqVPIS5d6'
-});
-signIn.renderEl({
-  el: '#widget-container'
-}, function success(res) {
-  if (res.status === 'SUCCESS') {
-    console.log(res)
+
+
+async function runApp() {
+  try {
+    const user = await Auth.signIn('evan@schweitzerlabs.com', 'Passowrd_123');
+    console.log(user)
     Elm.Main.init({
       node: document.getElementById('root')
     });
-  } else {
-    // The user can be in another authentication state that requires further action.
-    // For more information about these states, see:
-    //   https://github.com/okta/okta-signin-widget#rendereloptions-success-error
+  } catch (error) {
+    console.log('error signing in', error);
   }
-});
+}
+
+runApp();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
