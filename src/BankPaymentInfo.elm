64caeed7683af326264cf2932068cff0f72b9953
nelsonr.lopez <nelson@schweitzerlabs.com>
2021-07-07 13:02:28 -0400

    paymentinfo scaffolding

---
+++ src/BankPaymentInfo.elm
@@ -0,0 +1,138 @@
+module BankPaymentInfo exposing (MakeBankPaymentInfoConfig, view)
+
+import Asset
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Html exposing (Html, div, h4, h5, text)
+import Html.Attributes exposing (class)
+import PaymentMethod exposing (PaymentMethod)
+
+
+
+--- MODEL
+
+
+type alias MakeBankPaymentInfoConfig =
+    { amount : ( String, String )
+    , date : ( String, String )
+    , paymentType : ( String, PaymentMethod )
+    , ruleVerified : ( String, Bool )
+    , bankVerified : ( String, Bool )
+    , verificationScore : ( String, String )
+    }
+
+
+toDataString : PaymentMethod -> String
+toDataString method =
+    case method of
+        PaymentMethod.Ach ->
+            "Ach"
+
+        PaymentMethod.Wire ->
+            "Wire"
+
+        PaymentMethod.Check ->
+            "Check"
+
+        PaymentMethod.Debit ->
+            "Debit"
+
+        PaymentMethod.Credit ->
+            "Credit"
+
+        PaymentMethod.InKind ->
+            "InKind"
+
+        PaymentMethod.Transfer ->
+            "Transfer"
+
+        PaymentMethod.Other ->
+            "Other"
+
+
+statusContent : Bool -> Html msg
+statusContent val =
+    if val then
+        Asset.circleCheckGlyph [ class "text-green data-icon-size" ]
+
+    else
+        Asset.minusCircleGlyph [ class "text-warning data-icon-size" ]
+
+
+formLabel : String -> Html msg
+formLabel str =
+    h5 [] [ text str ]
+
+
+dataLabel : String -> Html msg
+dataLabel label =
+    h4 [ class "data-label" ] [ text label ]
+
+
+dataText : String -> Html msg
+dataText data =
+    h4 [ class "data-text" ] [ text data ]
+
+
+
+--- Todo refactor branching logic here
+
+
+labelWithData : ( String, String ) -> Html msg
+labelWithData ( label, data ) =
+    div []
+        [ dataLabel label
+        , dataText data
+        ]
+
+
+labelWithPaymentMethodData : ( String, PaymentMethod ) -> Html msg
+
+
+labelWithPaymentData ( label, paymentMethod ) =
+    div []
+        [ dataLabel label
+        , dataText <| toDataString paymentMethod
+        ]
+
+
+labelWithBankVerificationIcon : ( String, Bool ) -> Html msg
+labelWithBankVerificationIcon ( label, verificationStatus ) =
+    div []
+        [ dataLabel label
+        , statusContent verificationStatus
+        ]
+
+
+verificationInfoRow : MakeBankPaymentInfoConfig -> List (Html msg)
+verificationInfoRow model =
+    [ Grid.row []
+        [ Grid.col [ Col.md4 ] [ labelWithBankVerificationIcon model.ruleVerified ]
+        , Grid.col [ Col.md4 ] [ labelWithBankVerificationIcon model.bankVerified ]
+        , Grid.col [ Col.md4 ] [ labelWithData model.verificationScore ]
+        ]
+    ]
+
+
+paymentInfoRow : MakeBankPaymentInfoConfig -> List (Html msg)
+paymentInfoRow model =
+    [ Grid.row []
+        [ Grid.col [ Col.md4 ] [ labelWithData model.amount ]
+        , Grid.col [ Col.md4 ] [ labelWithData model.date ]
+        , Grid.col [ Col.md4 ] [ labelWithPaymentData model.paymentType ]
+        ]
+    ]
+
+
+
+---- View
+
+
+view : MakeBankPaymentInfoConfig -> Html msg
+view model =
+    Grid.containerFluid
+        []
+    <|
+        []
+            ++ paymentInfoRow model
+            ++ verificationInfoRow model
