99f04df352517b6a10cc1629b07b1ff6dcc90b29
nelsonr.lopez <nelson@schweitzerlabs.com>
2021-07-09 12:15:07 -0400

    reconfigured bankdata view to load a real transaction

--- src/BankData.elm
+++ src/BankData.elm
@@ -6,24 +6,19 @@ import Bootstrap.Grid.Col as Col
 import Bootstrap.Grid.Row as Row
 import Bootstrap.Utilities.Spacing as Spacing
 import Html exposing (Attribute, Html, h5, h6, text)
-import LabelWithData exposing (labelWithData, labelWithDescriptionData)
+import LabelWithData exposing (labelWithData, labelWithMaybeData, labelWithMaybeTimeData, labelWithTimeData)
 import PaymentMethod exposing (PaymentMethod)
+import TimeZone exposing (america__new_york)
+import Timestamp
+import Transaction
 
 
 
 --- Model ---
 
 
-type alias PaymentData =
-    { postedDate : String
-    , paymentType : PaymentMethod
-    }
-
-
 type alias Model =
-    { bankData : BankData
-    , paymentData : PaymentData
-    }
+    Transaction.Model
 
 
 formLabelRow : String -> List (Html msg)
@@ -33,25 +28,25 @@ formLabelRow str =
     ]
 
 
-bankInfoRows : BankData -> List (Html msg)
+bankInfoRows : Model -> List (Html msg)
 bankInfoRows data =
     [ Grid.row [ Row.attrs [ Spacing.mt2 ] ]
-        [ Grid.col [ Col.md4, Col.attrs [ Spacing.ml2 ] ] [ labelWithData "Analyzed Payee Name" data.analyzedPayeeName ]
-        , Grid.col [ Col.md4, Col.offsetLg3 ] [ labelWithData "Analyzed Category" data.analyzedCategory ]
+        [ Grid.col [ Col.md4, Col.attrs [ Spacing.ml2 ] ] [ labelWithMaybeData "Analyzed Payee Name" data.finicityNormalizedPayeeName ]
+        , Grid.col [ Col.md4, Col.offsetLg3 ] [ labelWithMaybeData "Analyzed Category" data.finicityCategory ]
         ]
     , Grid.row []
-        [ Grid.col [ Col.attrs [ Spacing.mt2, Spacing.ml2 ] ] [ labelWithDescriptionData "Description" data.description ] ]
+        [ Grid.col [ Col.attrs [ Spacing.mt2, Spacing.ml2 ] ] [ labelWithMaybeData "Description" data.finicityDescription ] ]
     , Grid.row []
-        [ Grid.col [ Col.md4, Col.attrs [ Spacing.mt2, Spacing.ml2 ] ] [ labelWithData "Initiated Date" data.analyzedTransactionDate ]
+        [ Grid.col [ Col.md4, Col.attrs [ Spacing.mt2, Spacing.ml2 ] ] [ labelWithMaybeTimeData "Initiated Date" data.finicityTransactionDate ]
         ]
     ]
 
 
-paymentInfoRow : PaymentData -> List (Html msg)
+paymentInfoRow : Model -> List (Html msg)
 paymentInfoRow data =
     [ Grid.row [ Row.attrs [ Spacing.mt2 ] ]
-        [ Grid.col [ Col.md4, Col.attrs [ Spacing.ml2 ] ] [ labelWithData "Posted Date" data.postedDate ]
-        , Grid.col [ Col.md4, Col.attrs [ Spacing.ml2 ] ] [ labelWithData "Payment Type" <| PaymentMethod.toDisplayString data.paymentType ]
+        [ Grid.col [ Col.md4, Col.attrs [ Spacing.ml2 ] ] [ labelWithTimeData "Posted Date" <| data.initiatedTimestamp ]
+        , Grid.col [ Col.md4, Col.attrs [ Spacing.ml2 ] ] [ labelWithData "Payment Type" <| PaymentMethod.toDisplayString data.paymentMethod ]
         ]
     ]
 
@@ -63,5 +58,5 @@ view model =
     <|
         []
             ++ formLabelRow "Bank Data"
-            ++ bankInfoRows model.bankData
-            ++ paymentInfoRow model.paymentData
+            ++ bankInfoRows model
+            ++ paymentInfoRow model
