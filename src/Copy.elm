822c879d81cba453a2e4960dddcb29bd72b70f40
nelsonr.lopez <nelson@schweitzerlabs.com>
2021-08-31 11:38:04 -0400

    added copy

--- src/Copy.elm
+++ src/Copy.elm
@@ -1,6 +1,8 @@
 module Copy exposing (contribUnverifiedDialogue, contribVerifiedDialogue, disbUnverifiedDialogue, disbVerifiedDialogue, verificationScore)
 
-import Html exposing (Html, text)
+import Bootstrap.Utilities.Spacing as Spacing
+import Html exposing (Html, br, div, span, text)
+import Html.Attributes exposing (class)
 
 
 verificationScore : String
@@ -10,27 +12,33 @@ verificationScore =
 
 disbUnverifiedDialogue : List (Html msg)
 disbUnverifiedDialogue =
-    [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-    , text "At urna condimentum mattis pellentesque id nibh tortor id aliquet."
+    [ span [ class "font-weight-bold" ] [ text "Instructions: " ]
+    , text " 4US has confirmed a payment left your bank account."
+    , text " Below you’ll see what information  was pulled from your bank statement."
+    , text " Please reconcile this transaction with the list of disbursements you pre-populated, or create a new record with the required data."
+    , text " This transaction will not show up on your disclosure report until the required compliance data is provided."
+    , div [ Spacing.mt3 ] [ text " If you run into any issues please email us at support@4US.net. Our team is ready to help." ]
     ]
 
 
 contribUnverifiedDialogue : List (Html msg)
 contribUnverifiedDialogue =
-    [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-    , text "At urna condimentum mattis pellentesque id nibh tortor id aliquet."
+    [ span [ class "font-weight-bold" ] [ text "Instructions: " ]
+    , text " 4US has confirmed a payment came into your bank account."
+    , text " Below you’ll see what information was pulled from your bank statement."
+    , text " Please reconcile this transaction with the list of contributions you pre-populated, or create a new record with the required data"
+    , text " This transaction will not show up on your disclosure report until the required compliance data is provided."
+    , div [ Spacing.mt3 ] [ text " If you run into any issues please email us at support@4US.net. Our team is ready to help." ]
     ]
 
 
 contribVerifiedDialogue : List (Html msg)
 contribVerifiedDialogue =
-    [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-    , text "At urna condimentum mattis pellentesque id nibh tortor id aliquet."
+    [ text "You are editing a record that has already been ID verified, reconciled and recorded."
     ]
 
 
 disbVerifiedDialogue : List (Html msg)
 disbVerifiedDialogue =
-    [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-    , text "At urna condimentum mattis pellentesque id nibh tortor id aliquet."
+    [ text "You are editing a record that has already been ID verified, reconciled and recorded."
     ]
