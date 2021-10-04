677f72d3dda06237e135a1faf426b8a25c4373cc
nelsonr.lopez <nelson@schweitzerlabs.com>
2021-10-04 13:29:56 -0400

    fixed disb test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -130,11 +130,11 @@ Cypress.Commands.add ('initDisb',()=>{
 Cypress.Commands.add('fillDisbForm', ()=>{
     cy.get('.container-fluid > .fade-in > .col > #recipient-name')
         .type(`${individual.firstName} ${individual.lastName}`)
-    cy.get(':nth-child(2) > :nth-child(1) > #addressLine1').type(individual.addressLine1)
-    cy.get(':nth-child(2) > :nth-child(2) > #addressLine2').type(individual.addressLine2)
-    cy.get(':nth-child(3) > .col-lg-6 > #city').type(individual.city)
-    cy.get(':nth-child(3) > :nth-child(2) > #State').select(individual.state)
-    cy.get(':nth-child(3) > :nth-child(3) > #postalCode').type(individual.postalCode)
+    cy.get('[data-cy=createDisbaddressLine1]').first().type(individual.addressLine1)
+    cy.get('[data-cy=createDisbaddressLine2]').first().type(individual.addressLine2)
+    cy.get('[data-cy=createDisbcity]').first().type(individual.city)
+    cy.get('[data-cy=createDisbstate]').first().select(individual.state)
+    cy.get('[data-cy=createDisbpostalCode]').first().type(individual.postalCode)
 
     cy.get(':nth-child(4) > .col > .form-group > #purpose').select(individual.purposeCode)
 
