5033fc35982b2af694ccf6778f927b40511f94f3
nelsonr.lopez <nelson@schweitzerlabs.com>
2021-12-08 14:17:39 -0500

    added support for explanation field on OTHER purpose codes

--- src/TxnForm/DisbRuleVerified.elm
+++ src/TxnForm/DisbRuleVerified.elm
@@ -52,6 +52,7 @@ type alias Model =
     , maybeError : Maybe String
     , formDisabled : Bool
     , popoverState : Popover.State
+    , explanation : String
     }
 
 
@@ -88,6 +89,7 @@ init txn =
     , isSubmitDisabled = True
     , maybeError = Nothing
     , popoverState = Popover.initialState
+    , explanation = Maybe.withDefault "" txn.explanation
     }
 
 
@@ -158,6 +160,7 @@ disbFormRow model =
         , toggleEdit = EditFormToggled
         , maybeError = model.maybeError
         , txnID = Just model.txn.id
+        , explanation = ( model.explanation, ExplanationUpdated )
         }
 
 
@@ -179,6 +182,7 @@ type Msg
     | PaymentDateUpdated String
     | PaymentMethodUpdated (Maybe PaymentMethod.Model)
     | CheckNumberUpdated String
+    | ExplanationUpdated String
     | EditFormToggled
     | BankDataToggled
 
@@ -230,6 +234,9 @@ update msg model =
 
         IsInKindUpdated bool ->
             ( { model | isInKind = bool }, Cmd.none )
+
+        ExplanationUpdated str ->
+            ( { model | explanation = str }, Cmd.none )
 
         BankDataToggled ->
             ( { model | showBankData = not model.showBankData }, Cmd.none )
