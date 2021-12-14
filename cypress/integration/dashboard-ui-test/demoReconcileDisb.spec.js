88949b867ba8270308cd3b545900b16b204fe315
nelsonr.lopez <nelson@schweitzerlabs.com>
2021-12-14 14:29:45 -0500

    added limit test for reconciliation

--- cypress/integration/dashboard-ui-test/demoReconcileDisb.spec.js
+++ cypress/integration/dashboard-ui-test/demoReconcileDisb.spec.js
@@ -23,6 +23,21 @@ describe("Demo Disbursement Reconciliation", () => {
     );
   });
 
+  it("It wont reconcile above the limit on disbursements", () => {
+    cy.createDemo();
+    cy.get("[data-cy=seedMoneyOut]").click();
+    cy.get(".col-12 > a").then((e) => {
+      cy.visit(e.text());
+    });
+    cy.fillReconcileDisb("500");
+    cy.fillReconcileDisb("500");
+
+    cy.get("tbody > :nth-child(1) > :nth-child(1)").click();
+    cy.get('[type="checkbox"]').check();
+
+    cy.get("[data-cy=disbRuleUnverifiedsubmitButton]").should("be.disabled");
+  });
+
   it("It can reconcile multiple contributions", () => {
     cy.createDemo();
     cy.get("[data-cy=seedMoneyIn]").click();
@@ -44,5 +59,19 @@ describe("Demo Disbursement Reconciliation", () => {
       "have.text",
       " Reconciliation Successful!"
     );
+  });
+
+  it("It wont reconcile above the limit on contributions", () => {
+    cy.createDemo();
+    cy.get("[data-cy=seedMoneyIn]").click();
+    cy.get(".col-12 > a").then((e) => {
+      cy.visit(e.text());
+    });
+    cy.fillReconcileContrib("1200");
+    cy.fillReconcileContrib("1200");
+    cy.get("tbody > :nth-child(1) > :nth-child(1)").click();
+    cy.get('[type="checkbox"]').check();
+
+    cy.get("[data-cy=contribRuleUnverifiedsubmitButton]").should("be.disabled");
   });
 });
